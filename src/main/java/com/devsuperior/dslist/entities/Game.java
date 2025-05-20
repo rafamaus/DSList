cce463c720a2481fdf1b9ece0847355cbb738f87
Rafael Maus <rafaellmaus06@gmail.com>
2025-05-20 09:32:38 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -20,23 +20,27 @@ public class Game {
 	
 	@Column(name = "game_year")
 	private Integer year;
-	private String genrr;
+	private String genre;
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private	String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genrr, String platforms, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
-		this.genrr = genrr;
+		this.genre = genre;
 		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
@@ -68,12 +72,12 @@ public class Game {
 		this.year = year;
 	}
 
-	public String getGenrr() {
-		return genrr;
+	public String getGenre() {
+		return genre;
 	}
 
-	public void setGenrr(String genrr) {
-		this.genrr = genrr;
+	public void setGenre(String genre) {
+		this.genre = genre;
 	}
 
 	public String getPlatforms() {
